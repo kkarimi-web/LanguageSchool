2ba6395a863ea47f6f2355c2a122c29bbbea55d5
Karimi-PC <Karimi-PC@DESKTOP-PFMSBBN>
2020-08-20 00:15:16 +0330

    Add Course Model,Service,presentation Details

---
+++ CleanArch/CleanArch.Application/Validations/CourseValidation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Domain.Models;
+using FluentValidation;
+
+
+namespace CleanArch.Application.Validations
+{
+    public class CourseValidation : AbstractValidator<Course>
+    {
+        public CourseValidation()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Course Name is Required");
+            RuleFor(f => f.ImageUrl).NotEmpty().When(c => c.Name.Contains("Summit")).WithMessage("Image Url is Required");
+        }
+    }
+}
