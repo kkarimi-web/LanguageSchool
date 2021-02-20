86768f2af9beb9e68ade00f4a2873cd8a6d20fa2
Karimi-PC <Karimi-PC@DESKTOP-PFMSBBN>
2021-02-20 14:36:38 +0330

    Dependancy container

--- CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -4,6 +4,7 @@ using CleanArch.Application.Services;
 using CleanArch.Application.Validations;
 using CleanArch.Domain.Interfaces;
 using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Infrastructure;
 using CleanArch.Infra.Data.Repository;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,10 +19,18 @@ namespace CleanArch.Infra.IoC
         public static void RegisterServices(IServiceCollection services)
         {
             //Application layer
-            services.AddScoped<ICourseService, CourseService>();        
+            services.AddScoped<ICourseService, CourseService>();
+            services.AddScoped<ICourseTypeService, CourseTypeService>();
+            services.AddScoped<IFileRepositoryService, FileRepositoryService>();
+            services.AddScoped<ICourseDocumentService, CourseDocumentServie>();
 
             //Infra.Data Layer
             services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<ICourseTypeRepository, CourseTypeRepository>();
+            services.AddScoped<IFileRepository, FileRepository>();
+            services.AddScoped<ICourseDocumentRepository, CourseDocumentRepository>();
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped<IUnitOfWork,UnitOfWork>();
 
             //Domain layer
             services.AddSingleton<IValidator<Course>, CourseValidation>();
